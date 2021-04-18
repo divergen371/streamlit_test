bcfbb00dec247c33e3bb9398ce01b053e588d350
divergen371 <divergen@protonmail.com>
2021-04-18 15:23:12 +0900

    update

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import time
 
-st.title('Streamlit')
+st.title("Streamlit")
 
-st.write('DataFrame')
+st.write("Progress Bar")
+"Start!!"
+latest_iteration = st.empty()
+bar = st.progress(0)
+for i in range(100):
+    latest_iteration.text(f"iteration {i+1}")
+    bar.progress(i + 1)
+    time.sleep(0.1)
 
-df = pd.DataFrame({
-    '1列目': [1,2,3,4],
-    '2列目': [10,20,30,40]
-})
+"Done!!"
+left_col, right_col = st.beta_columns(2)
+button = left_col.button("右カラムに文字を表示")
+if button:
+    right_col.write("Right cols is here.")
 
-st.write(df)
 
-st.dataframe(df.style.highlight_max(axis=0))
-st.table(df.style.highlight_max(axis=0))
-
+expander1 = st.beta_expander("問い合わせ1")
+expander1.write("問い合わせ内容を記入してください。")
+expander2 = st.beta_expander("問い合わせ2")
+expander2.write("問い合わせ内容を記入してください。")
+expander3 = st.beta_expander("問い合わせ3")
+expander3.write("問い合わせ内容を記入してください。")
+# text = st.text_input("あなたの趣味を教えて下さい")
+# "あなたの趣味:", text
+#
+# condition = st.slider("あなたの今のコンディションは？", 0, 100, 50)
+# "コンディション:", condition
